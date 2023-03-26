3ee59000326486568ee1bceb8fc89b441ba16858
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-03-26 15:06:21 -0400

    Added toDoContainer

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const inputField = document.querySelector(".inputField");
 const btn = document.querySelector(".btn");
 const toDoContainer = document.querySelector(".toDoContainer");
 
-btn.addEventListener("click", function () {});
+btn.addEventListener("click", function () {
+	const item = document.createElement('li');
+	item.innerText = inputField.value;
+	item.classList.add('toDoAdded');
+	toDoContainer.appendChild(item);
+	console.log(item);
+});
