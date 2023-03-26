17d3ff123f43180dba9e5fc7b074801ffded4a65
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-03-26 11:13:14 -0400

    Added JS file

---
+++ index.js
@@ -0,0 +1,5 @@
+const inputField = document.querySelector(".inputField");
+const btn = document.querySelector(".btn");
+const toDoContainer = document.querySelector(".toDoContainer");
+
+btn.addEventListener("click", function () {});
